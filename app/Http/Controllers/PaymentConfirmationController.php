babb397f34102c1ec2973220250b2cc513ad2ac1
maustem <diego.maustem@gmail.com>
2024-12-18 14:57:52 -0300

    :recycle: refactor: Implementing try and catch

--- app/Http/Controllers/PaymentConfirmationController.php
+++ app/Http/Controllers/PaymentConfirmationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Jobs\PaymentConfirmationJob;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PaymentConfirmationController extends Controller
 {
@@ -11,6 +12,15 @@ class PaymentConfirmationController extends Controller
     {
         $paymentDetails = $request->all();
 
-        PaymentConfirmationJob::dispatch($paymentDetails)->delay(now()->addSeconds(3))->onQueue('PaymentConfirmationJob');
+        try {
+            PaymentConfirmationJob::dispatch($paymentDetails)->delay(now()->addSeconds(3))->onQueue('PaymentConfirmationJob');
+
+            return response()->json(['message' => 'Payment successfully!'], 200);
+        } catch(\Exception $e) {
+            // Caso exista algum error, armazenaremos em log ::: 
+            Log::error('An error occurred: ' . $e->getMessage());
+            
+            return response()->json(['error' => 'Payment error!'], 500);
+        }
     }
 }
